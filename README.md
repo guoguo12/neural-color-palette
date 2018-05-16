079b582b68da4b9d9091cf5fdf468b8dd69cf267
Allen Guo <guoguo12@gmail.com>
2018-05-16 15:25:59 -0700

    Plot two separate images, make predictions in batches

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
-Crayon colors often have interesting names: official [Crayola colors](https://en.wikipedia.org/wiki/List_of_Crayola_crayon_colors) include "Inchworm", "Shamrock", "Pink Flamingo", and "Eggplant".
-I used a neural network trained on ImageNet to generate better names for the [X11 colors](https://en.wikipedia.org/wiki/X11_color_names).
+# Neural Color Palette
+
+Colors are often named after objects: official [Crayola colors](https://en.wikipedia.org/wiki/List_of_Crayola_crayon_colors) include "Inchworm", "Shamrock", "Pink Flamingo", and "Eggplant".
+I used a ResNet50 neural network trained on ImageNet to generate better names for the [X11 colors](https://en.wikipedia.org/wiki/X11_color_names).
 
 Here are the results:
 
-![results](https://raw.githubusercontent.com/guoguo12/neural-crayon-pack/master/result.png)
+![](https://raw.githubusercontent.com/guoguo12/neural-crayon-pack/master/grid.png)
 
 The large text over each color is the predicted class, the number below it is the prediction probability, and the small text above the color is the color's official X11 name.
 
-(The data for the X11 colors comes from the official X source code [here](https://cgit.freedesktop.org/xorg/app/rgb/tree/rgb.txt).)
+The are 19 unique predicted color names, shown here with their corresponding maximum-posterior colors:
+
+![](https://raw.githubusercontent.com/guoguo12/neural-crayon-pack/master/uniques.png)
+
+## Acknowledgements
+
+The data for the X11 colors comes from the official X source code [here](https://cgit.freedesktop.org/xorg/app/rgb/tree/rgb.txt).
--- main.py
+++ main.py
@@ -4,10 +4,9 @@ from keras.applications.resnet50 import ResNet50
 from keras.applications.resnet50 import preprocess_input, decode_predictions
 import matplotlib.pyplot as plt
 import numpy as np
-from tqdm import tqdm
 
 
-def get_X11_colors():
+def get_x11_colors():
     colors = {}
     with open('rgb.txt') as f:
         for line in f.readlines():
@@ -22,43 +21,74 @@ def get_X11_colors():
     return colors
 
 
-def predict(color, model=ResNet50(weights='imagenet')):
-    x = np.broadcast_to(color, (1, 224, 224, 3)).astype(float)
-    x = preprocess_input(x)
-    return decode_predictions(model.predict(x), top=1)[0][0][1:]
+def predict(colors, model=ResNet50(weights='imagenet')):
+    # Reshape colors from (N, 3) to (N, 1, 1, 3), and then to (N, 224, 224, 3)
+    colors_array = np.array(colors)[:, np.newaxis, np.newaxis, :]
+    colors_array = np.broadcast_to(colors_array,
+                                   (len(colors), 224, 224, 3)).astype(float)
+
+    x = preprocess_input(colors_array)
+    predictions = decode_predictions(model.predict(x, verbose=1), top=1)
+    return [(color, name, probability) \
+            for (color, [(_, name, probability)]) in zip(colors, predictions)]
+
+
+def plot_color(ax, color_value, x11_name, predicted_name, probability):
+    ax.set_title(x11_name, size=12)
+    ax.set_facecolor(tuple(float(value) / 255 for value in color_value))
+
+    # Use heuristic to choose text color
+    # Source: https://stackoverflow.com/a/3943023/7911713
+    r, g, b = color_value
+    text_color = 'k' if r * 0.299 + g * 0.587 + b * 0.116 > 186 else 'w'
+
+    ax.text(0.5, 0.5, predicted_name,
+            color=text_color, size=20,
+            horizontalalignment='center', verticalalignment='center',
+            transform=ax.transAxes)
+    ax.text(0.5, 0.2, '{:.3f}'.format(probability),
+            color=text_color, size=12,
+            horizontalalignment='center', verticalalignment='center',
+            transform=ax.transAxes)
+
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+
+def plot_grid(results, x11_names):
+    fig, axes = plt.subplots(24, 6, figsize=(28, 40))
+    for (ax, x11_name, (color_value, predicted_name, probability)) \
+        in zip(axes.flatten(), x11_names, results):
+        plot_color(ax, color_value, x11_name, predicted_name, probability)
+
+    plt.subplots_adjust(hspace=0.5)
+    plt.savefig('grid.png', bbox_inches='tight', pad_inches=0.5)
+
+
+def plot_uniques(results):
+    unique_predicted_names = set(predicted_name \
+                                 for (_, predicted_name, _) in results)
+
+    fig, axes = plt.subplots(len(unique_predicted_names), 1, figsize=(12, 40))
+    for (ax, predicted_name) in zip(axes, sorted(unique_predicted_names)):
+        matching_results = [r for r in results if r[1] == predicted_name]
+        color_value, _, probability = max(matching_results, key=lambda r: r[2])
+        plot_color(ax, color_value, '', predicted_name, probability)
+
+    plt.subplots_adjust(hspace=0.5)
+    plt.savefig('uniques.png', bbox_inches='tight', pad_inches=0.5)
 
 
 def main():
-    colors = get_X11_colors()
-    sorted_colors = sorted(colors.items(), key=lambda x: x[1])
-    assert len(sorted_colors) >= 144  # We'll only use 144 colors (18 x 8 grid)
+    colors = list(get_x11_colors().items())
+    colors.sort(key=lambda x: x[1])  # Sort by RGB values
+    x11_names, color_values = zip(*colors)
 
-    fig, axes = plt.subplots(18, 8, figsize=(24, 30))
-    for (ax, (color_name, color_value)) in tqdm(list(zip(axes.flatten(),
-                                                         sorted_colors))):
-        ax.set_facecolor(tuple(float(value) / 255 for value in color_value))
+    results = predict(color_values)
 
-        # Use heuristic to choose text color
-        # Source: https://stackoverflow.com/a/3943023/7911713
-        r, g, b = color_value
-        text_color = 'k' if r * 0.299 + g * 0.587 + b * 0.116 > 186 else 'w'
+    plot_grid(results, x11_names)
+    plot_uniques(results)
 
-        predicted_name, prediction_posterior = predict(color_value)
-        ax.text(0.5, 0.5, predicted_name,
-                color=text_color, size=20,
-                horizontalalignment='center', verticalalignment='center',
-                transform=ax.transAxes)
-        ax.text(0.5, 0.25, '{:.3f}'.format(prediction_posterior),
-                color=text_color, size=12,
-                horizontalalignment='center', verticalalignment='center',
-                transform=ax.transAxes)
-        ax.set_title(color_name, size=12)
-
-        ax.get_xaxis().set_visible(False)
-        ax.get_yaxis().set_visible(False)
-
-    plt.subplots_adjust(hspace=0.5)
-    plt.savefig('result.png', bbox_inches='tight', pad_inches=1.5)
 
 if __name__ == '__main__':
     main()
